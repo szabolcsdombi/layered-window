0563bb401bfbbf6d8c89046f42f3cc9ab4fb81ee
Szabolcs Dombi <szabolcs@szabolcsdombi.com>
2024-03-10 16:09:46 +0200

    fix stubs

--- layered_window-stubs/__init__.pyi
+++ layered_window-stubs/__init__.pyi
@@ -2,10 +2,10 @@ from typing import Tuple
 
 def init(
     size: Tuple[int, int],
-    position: Tuple[int, int],
+    position: Tuple[int, int] = (0, 0),
     disable_move: bool = False,
     always_on_top: bool = False,
     tool_window: bool = False
-): ...
+) -> memoryview: ...
 
-def update(multiply_alpha: bool = True, flush: bool = True): ...
+def update(multiply_alpha: bool = True, flush: bool = True) -> None: ...
